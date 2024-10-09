4e53e2640d197f7644b7b93f817ab7a082933be4
Your Name <you@example.com>
2024-10-09 13:37:08 +0530

    l

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # testdeploy
-1
+
---
+++ python/app.py
@@ -0,0 +1,10 @@
+# app.py
+
+def main():
+    print("Hello, World!")
+    print("This is a simple Python application.")
+    print("Welcome to Jenkins CI/CD!")
+
+if __name__ == "__main__":
+    main()
+
